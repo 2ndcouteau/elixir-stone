efcf342a9e28fbac7a416c25116387541d29a8d4
2ndcouteau <17924220+2ndcouteau@users.noreply.github.com>
2019-04-17 23:14:08 +0200

    feat: Init first project

---
+++ lib/elixir_stone.ex
@@ -0,0 +1,18 @@
+defmodule ElixirStone do
+  @moduledoc """
+  Documentation for ElixirStone.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirStone.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
