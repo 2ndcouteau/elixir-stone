8e4d921cd3c19138b349662241c3a5870da92abb
2ndcouteau <17924220+2ndcouteau@users.noreply.github.com>
2019-04-25 22:02:41 +0200

    feat: Add new exercice following MIX AND OTP documentation elixir-lang

---
+++ z_tuto/kv/test/kv_test.exs
@@ -0,0 +1,12 @@
+defmodule KVTest do
+  use ExUnit.Case
+  doctest KV
+
+  test "greets the world" do
+    assert KV.hello() == :world
+  end
+
+  test "Oops" do
+    assert KV.hello() != :oops
+  end
+end
