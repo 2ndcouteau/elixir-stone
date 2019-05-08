a139f20dbe90b30ae4f6ccb716eb1a6cadd91693
2ndcouteau <17924220+2ndcouteau@users.noreply.github.com>
2019-05-08 22:21:08 +0200

    test: add test for wallet_creation

--- test/fs_test.exs
+++ test/fs_test.exs
@@ -9,21 +9,34 @@ defmodule FSTest do
   test "Create Client with name", %{registry: _registry} do
     assert FS.Registry.fetch(Register, "toto") == []
 
-    assert {client_pid, id} = FS.cc("toto", 986, 4242)
+    assert {client_pid, id} = FS.create_client("toto", 986, 4242)
 
     assert FS.Clients.get(client_pid, :name) == "toto"
     assert FS.Clients.get(client_pid, :id) == id
     assert FS.Clients.get(client_pid, :main_currency) == 986
-    assert FS.Clients.get(client_pid, :wallet) == {986, 4242}
-    Supervisor.restart_child(FS.Supervisor, Register)
+    assert FS.Clients.get(client_pid, :wallets) == %{986 => 4242}
+    assert FS.delete_client(id)
+    Supervisor.terminate_child(FS.Supervisor, Register)
   end
 
   test "Delete client account", %{registry: _registry} do
-    assert {client_pid, id} = FS.cc("toto")
+    assert {client_pid, id} = FS.create_client("toto")
 
     assert FS.Registry.fetch(Register, id) != []
     assert FS.delete_client(id)
     assert FS.Registry.fetch(Register, id) == []
-    Supervisor.restart_child(FS.Supervisor, Register)
+    Supervisor.terminate_child(FS.Supervisor, Register)
+  end
+
+  test "Create New wallet", %{registry: _registry} do
+    assert {client_pid, id} = FS.create_client("toto")
+    assert FS.Clients.get(client_pid, :wallets) == %{986 => 0}
+    assert {id, currency, amount_deposited} = FS.create_wallet(id, 978, 1234)
+
+    assert FS.Clients.get(client_pid, :wallets)
+           |> Map.get(978) == 1234
+
+    assert FS.delete_client(id)
+    Supervisor.terminate_child(FS.Supervisor, Register)
   end
 end
