8daca99c47b885173f12bbb74f5717b54fe3211e
2ndcouteau <17924220+2ndcouteau@users.noreply.github.com>
2019-05-08 16:53:02 +0200

    test: Add test for delete_client() Fix the context of create_client test

--- test/fs_test.exs
+++ test/fs_test.exs
@@ -6,15 +6,24 @@ defmodule FSTest do
     %{registry: registry}
   end
 
-  test "Create Client with name", %{registry: registry} do
-    assert FS.Registry.fetch(registry, "toto") == []
+  test "Create Client with name", %{registry: _registry} do
+    assert FS.Registry.fetch(Register, "toto") == []
 
-    assert {client_pid, id} = FS.Registry.create_client(registry, "toto")
-    assert FS.Clients.put(client_pid, "toto", id, 986, 4242)
+    assert {client_pid, id} = FS.cc("toto", 986, 4242)
 
     assert FS.Clients.get(client_pid, :name) == "toto"
     assert FS.Clients.get(client_pid, :id) == id
     assert FS.Clients.get(client_pid, :main_currency) == 986
     assert FS.Clients.get(client_pid, :wallet) == {986, 4242}
+    Supervisor.restart_child(FS.Supervisor, Register)
+  end
+
+  test "Delete client account", %{registry: _registry} do
+    assert {client_pid, id} = FS.cc("toto")
+
+    assert FS.Registry.fetch(Register, id) != []
+    assert FS.delete_client(id)
+    assert FS.Registry.fetch(Register, id) == []
+    Supervisor.restart_child(FS.Supervisor, Register)
   end
 end
