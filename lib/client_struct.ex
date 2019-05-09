959b6bd48a6c3a0c37609abe96866fd9ab45f48c
2ndcouteau <17924220+2ndcouteau@users.noreply.github.com>
2019-05-09 19:49:24 +0200

    fix: Change id default value type from String.t to integer

--- lib/client_struct.ex
+++ lib/client_struct.ex
@@ -1,6 +1,6 @@
 defmodule ClientStruct do
   defstruct name: "unknown",
-            id: "0001000",
+            id: 1000,
             # {CODE 986 == NAME "BRL"}
             main_currency: 986,
             wallets: %{986 => 0}
