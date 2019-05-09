2c103a683810f6391bf457fb4ed0ffed25f39908
2ndcouteau <17924220+2ndcouteau@users.noreply.github.com>
2019-05-09 22:11:37 +0200

    feat: add decimal_arithmetic to deps for float calculation

--- mix.exs
+++ mix.exs
@@ -24,7 +24,8 @@ defmodule FinancialSystem.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false},
+      {:decimal_arithmetic, "~> 0.1.2"}
       # {:named_args, "~> 0.1.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
