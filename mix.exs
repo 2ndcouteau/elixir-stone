9399987b0438e196a34d16ddb32854493df06a28
2ndcouteau <17924220+2ndcouteau@users.noreply.github.com>
2019-05-10 20:05:30 +0200

    refacto: Remove useless comments

--- mix.exs
+++ mix.exs
@@ -28,8 +28,6 @@ defmodule FinancialSystem.MixProject do
       {:decimal_arithmetic, "~> 0.1.2"},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false}
       # {:named_args, "~> 0.1.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 
