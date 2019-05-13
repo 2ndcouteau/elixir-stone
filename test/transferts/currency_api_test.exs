e796cc1f5e68e25063c8463b9599c49af6ef7b54
2ndcouteau <17924220+2ndcouteau@users.noreply.github.com>
2019-05-13 22:11:22 +0200

    test: Full unit_tests for FS.Transfert and Currency_API

--- test/transferts/currency_api_test.exs
+++ test/transferts/currency_api_test.exs
@@ -1,17 +1,91 @@
 defmodule Currency_APITest do
   use ExUnit.Case
+  use FS.Path_Resources
 
   setup do
     registry = start_supervised!(FS.Transfer)
     %{registry: registry}
   end
 
-  test "Get available currencies", %{registry: _registry} do
-    assert iso_ref = Currency_API.get_iso_ref()
-    assert last_conversions = Currency_API.get_last_conversions()
-    assert available_currencies = Currency_API.get_available_currencies(iso_ref, last_conversions)
+  # Direct request to the Fixer.io API
+  # !!! Credit limited to 1000 requests by month
+  @tag :external
+  test "Get exchange rates from API", %{registry: _registry} do
+    req = Currency_API.get_exchange_rates()
+
+    assert Map.get(req, "success") == true
+    assert is_integer(Map.get(req, "timestamp")) == true
+    assert Map.get(req, "base") == "EUR"
+    assert is_binary(Map.get(req, "date")) == true
+    assert is_map(Map.get(req, "rates")) == true
+  end
+
+  # Direct request to the Fixer.io API
+  # !!! Credit limited to 1000 requests by month
+  @tag :external
+  test "Update the rescue conversion rates file", %{registry: _registry} do
+    req = Currency_API.get_exchange_rates()
+
+    assert Currency_API.update_rescue_conversion_rates(req) == :ok
+  end
+
+  # Direct request to the Fixer.io API
+  # !!! Credit limited to 1000 requests by month
+  @tag :external
+  test "Init last conversions", %{registry: _registry} do
+    # Read/Write Error
+    # Other errors are managed in the same way
+    File.chmod(@last_conversions, 0o000)
+
+    assert ExUnit.Assertions.catch_exit(Currency_API.init_last_conversions()) ==
+             "Read: last_conversion.json: permission denied"
+
+    File.chmod(@last_conversions, 0o644)
+
+    # File does not exist
+    File.rm(@last_conversions)
+    assert is_map(Currency_API.init_last_conversions()) == true
+    assert File.exists?(@last_conversions) == true
+
+    # Regular File
+    assert is_map(Currency_API.init_last_conversions()) == true
+  end
+
+  test "Init iso_ref", %{registry: _registry} do
+    # Read/Write Error
+    # Other errors are managed in the same way
+    File.chmod(@iso_ref, 0o000)
+
+    assert ExUnit.Assertions.catch_exit(Currency_API.init_iso_ref()) ==
+             "Read: ISO_4217_reference.json: permission denied"
+
+    File.chmod(@iso_ref, 0o644)
+
+    # Regular File
+    assert is_list(Currency_API.init_iso_ref()) == true
+  end
+
+  test "Init available currencies", %{registry: registry} do
+    iso_ref = FS.Transfer.get_iso_ref(registry)
+    last_conversions = FS.Transfer.get_last_conversions(registry)
+
+    assert is_list(Currency_API.init_available_currencies(iso_ref, last_conversions)) == true
+  end
+
+  test "Number of available currencies", %{registry: _registry} do
+    assert iso_ref = Currency_API.init_iso_ref()
+    assert last_conversions = Currency_API.init_last_conversions()
+
+    assert available_currencies =
+             Currency_API.init_available_currencies(iso_ref, last_conversions)
 
     assert length(available_currencies) == 154
+  end
+
+  test "Get all Json", %{registry: _registry} do
+    assert is_list(Currency_API.get_all_json(@iso_ref)) == true
+
+    assert Currency_API.get_all_json("Bad/Path") == {:error, :enoent}
   end
 
   # test "Conversion currencies", %{registry: _registry} do
--- test/transferts/transfert_test.exs
+++ test/transferts/transfert_test.exs
@@ -6,13 +6,80 @@ defmodule FS.TransferTest do
     %{registry: registry}
   end
 
-  # test "Get all currency codes couple", %{registry: registry} do
-  #   assert FS.Transfert.get_all_code(registry)
-  # end
-  #
   test "Get one currency code couple", %{registry: registry} do
     assert FS.Transfer.get_one_code(registry, "USD") == {"840", "USD", "2"}
     assert FS.Transfer.get_one_code(registry, "840") == {"840", "USD", "2"}
     assert FS.Transfer.get_one_code(registry, "000") == {:error, "Currency unavailable"}
+  end
+
+  test "Get currency infos", %{registry: registry} do
+    available_currencies = FS.Transfer.get_available_currencies(registry)
+
+    assert FS.Transfer.get_currency_infos("USD", available_currencies) == {"840", "USD", "2"}
+    assert FS.Transfer.get_currency_infos("840", available_currencies) == {"840", "USD", "2"}
+    assert FS.Transfer.get_currency_infos("ABC", available_currencies) == nil
+    assert FS.Transfer.get_currency_infos("000", available_currencies) == nil
+  end
+
+  test "Get one code", %{registry: registry} do
+    assert FS.Transfer.get_one_code(registry, "840") == {"840", "USD", "2"}
+    assert FS.Transfer.get_one_code(registry, "USD") == {"840", "USD", "2"}
+    assert FS.Transfer.get_one_code(registry, "EUR") == {"978", "EUR", "2"}
+    assert FS.Transfer.get_one_code(registry, "978") == {"978", "EUR", "2"}
+
+    assert FS.Transfer.get_one_code(registry, "123") == {:error, "Currency unavailable"}
+    assert FS.Transfer.get_one_code(registry, "ABC") == {:error, "Currency unavailable"}
+
+    assert FS.Transfer.get_one_code(registry, :USD) == {:error, "Currency unavailable"}
+    assert FS.Transfer.get_one_code(registry, 840) == {:error, "Currency unavailable"}
+  end
+
+  test "Get one rate from last_conversions state", %{registry: registry} do
+    last_conversions = FS.Transfer.get_last_conversions(registry)
+
+    assert FS.Transfer.get_one_rate(registry, "BOB") ==
+             Map.get(last_conversions, "rates")
+             |> Map.get("BOB")
+
+    assert FS.Transfer.get_one_rate(registry, "068") ==
+             Map.get(last_conversions, "rates")
+             |> Map.get("BOB")
+  end
+
+  # Direct request to the Fixer.io API
+  # !!! Credit limited to 1000 requests by month
+  @tag :external
+  test "Get one rate from Fixer API", %{registry: registry} do
+    last_conversions = Currency_API.get_exchange_rates()
+
+    assert FS.Transfer.get_one_rate(registry, "BOB") ==
+             Map.get(last_conversions, "rates")
+             |> Map.get("BOB")
+
+    assert FS.Transfer.get_one_rate(registry, "068") ==
+             Map.get(last_conversions, "rates")
+             |> Map.get("BOB")
+  end
+
+  test "Get minor unit", %{registry: registry} do
+    assert FS.Transfer.get_minor_unit(registry, "USD") == "2"
+    assert FS.Transfer.get_minor_unit(registry, "EUR") == "2"
+
+    assert FS.Transfer.get_minor_unit(registry, "840") == "2"
+    assert FS.Transfer.get_minor_unit(registry, "068") == "2"
+
+    assert FS.Transfer.get_minor_unit(registry, "XOF") == "0"
+    assert FS.Transfer.get_minor_unit(registry, "952") == "0"
+
+    assert FS.Transfer.get_minor_unit(registry, "123") == {:error, "Currency unavailable"}
+    assert FS.Transfer.get_minor_unit(registry, "ABC") == {:error, "Currency unavailable"}
+  end
+
+  test "Conversion rates need to be updated ?", %{registry: registry} do
+    assert FS.Transfer.conversion_rates_up?(registry) == false
+  end
+
+  test "Get base currency for transfer from Fixer.io API", %{registry: registry} do
+    assert FS.Transfer.get_base(registry) == "EUR"
   end
 end
