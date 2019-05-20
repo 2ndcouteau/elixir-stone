9f4a1adf2777f89feb4b5d90361e346c6dd3aa02
2ndcouteau <17924220+2ndcouteau@users.noreply.github.com>
2019-05-20 07:02:03 +0200

    tests: Test transfer() and multi-transfer() done

--- test/transferts/transfert_test.exs
+++ test/transferts/transfert_test.exs
@@ -90,12 +90,12 @@ defmodule FS.TransferTest do
     assert {client_pid3, id3} = FS.create_client("tata", 978, 105_252.00)
 
     # Simple transfer
-    assert FS.Transfer.transfer(id1, id2, 986, 4242, true) == {"986", ~m(105252.00), ~m(4242.00)}
+    assert FS.Transfer.transfer(id1, id2, 986, 4242, true) == [:ok]
     assert FS.Clients.get_one_wallet_infos(id1, "986") == {"986", ~m(0.00)}
     assert FS.Clients.get_one_wallet_infos(id2, "986") == {"986", ~m(105252.00)}
 
     # No direct conversion
-    assert FS.Transfer.transfer(id3, id1, 978, 4242, false) == {"978", ~m(4242.00), ~m(4242.00)}
+    assert FS.Transfer.transfer(id3, id1, 978, 4242, false) == [:ok]
     # Check main_currency wallet
     assert FS.Clients.get_one_wallet_infos(id1, "986") == {"986", ~m(0.00)}
     # Check new currency wallet
@@ -112,8 +112,8 @@ defmodule FS.TransferTest do
     assert FS.Transfer.transfer(id1, id2, 986, 4242, true) == :error
 
     # Unknown client ID
-    assert FS.Transfer.transfer(3000, id2, 986, 4242, true) == :error
-    assert FS.Transfer.transfer(id1, 3000, 986, 4242, true) == :error
+    assert FS.Transfer.transfer(6000, id2, 986, 4242, true) == :error
+    assert FS.Transfer.transfer(id1, 8000, 986, 4242, true) == :error
 
     # No value transfered
     assert FS.Transfer.transfer(id1, id2, 986, 0, true) == :error
@@ -139,15 +139,13 @@ defmodule FS.TransferTest do
     ## Valid Tests
 
     # Different clients, same currency
-    assert FS.Transfer.transfer(id1 + 986, id2 + 986, 4242) ==
-             {"986", ~m(105252.00), ~m(4242.00)}
+    assert FS.Transfer.transfer(id1 + 986, id2 + 986, 4242) == [:ok]
 
     # Different clients, different currency
     assert FS.Transfer.transfer(id3 + 978, id1 + 986, 4242) != :error
 
     # Same client, same currency
-    assert FS.Transfer.transfer(id1 + 986, id1 + 986, 4242) ==
-             {"986", ~m(4242.00), ~m(4242.00)}
+    assert FS.Transfer.transfer(id1 + 986, id1 + 986, 4242) == [:ok]
 
     # Same client, different currency
     assert FS.Transfer.transfer(id1 + 986, id1 + 978, 4242) != :error
@@ -183,8 +181,7 @@ defmodule FS.TransferTest do
     assert FS.Transfer.transfer(id1, 124, 986, 424) != :error
 
     # Same currency
-    assert FS.Transfer.transfer(id1, 124, 124, 200) ==
-             {"124", ~m(3818.00), ~m(200.00)}
+    assert FS.Transfer.transfer(id1, 124, 124, 200) == [:ok]
 
     # With named currency
     assert FS.Transfer.transfer(id1, "BRL", 124, 42) != :error
@@ -194,19 +191,112 @@ defmodule FS.TransferTest do
     ## Error Tests
 
     # Bad currency
-    assert FS.Transfer.transfer(id1, 123, 124, 200) == :error
-    assert FS.Transfer.transfer(id1, 124, 123, 200) == :error
+    assert FS.Transfer.transfer(id1, 123, 124, 20) == :error
+    assert FS.Transfer.transfer(id1, 124, 123, 20) == :error
 
     assert FS.Transfer.transfer(id1, "ABC", 124, 200) == :error
     assert FS.Transfer.transfer(id1, 124, "ABC", 200) == :error
 
     # Bad amount
     assert FS.Transfer.transfer(id1, 124, 124, -200) == :error
+    assert FS.Transfer.transfer(id1, 124, 124, 9200) == :error
 
     # Unknown client
     assert FS.Transfer.transfer(8000, 124, 124, 200) == :error
 
     assert FS.delete_client(id1)
+    Supervisor.terminate_child(FS.Supervisor, Register)
+  end
+
+  test "Multi_Transfer/5 ", %{registry: _registry} do
+    assert {client_pid1, id1} = FS.create_client("toto", 986, 4242)
+    assert {client_pid2, id2} = FS.create_client("titi", 986, 101_010)
+    assert {client_pid3, id3} = FS.create_client("tata", 978, 105_252.00)
+    assert {client_pid2, id4} = FS.create_client("tutu", 986, 101_010)
+
+    # Simple transfer
+    assert FS.Transfer.multi_transfer(id1, [id2, id3, id4], 986, 4242, true) == [:ok, :ok, :ok]
+    assert FS.Clients.get_one_wallet_infos(id1, "986") == {"986", ~m(0.00)}
+
+    # No direct conversion
+    assert FS.Transfer.multi_transfer(id3, [id1, id2, id4], 978, 1000, false) == [:ok, :ok, :ok]
+    # Check main_currency wallet
+    assert FS.Clients.get_one_wallet_infos(id1, "986") == {"986", ~m(0.00)}
+    # Check new currency wallet
+    assert {currency, value} = FS.Clients.get_one_wallet_infos(id1, "978")
+    assert currency == "978"
+    assert Decimal.cmp(value, 0) == :gt
+
+    # Check debit from id3 wallet
+    assert FS.Clients.get_one_wallet_infos(id3, "978") != nil
+
+    # Unknown wallet
+    assert FS.Clients.get_one_wallet_infos(id3, "986") == nil
+
+    # Not enough founds
+    assert FS.Transfer.multi_transfer(id1, [id2], 986, 4242, true) == :error
+
+    # Unknown client ID
+    assert FS.Transfer.multi_transfer(46000, [id1, id2, id3], 986, 4242, true) == :error
+    assert FS.Transfer.multi_transfer(id1, [id2, 8000, id3, id4], 986, 4242, true) == :error
+
+    # No value transfered
+    assert FS.Transfer.multi_transfer(id1, [id2, id3, id4], 986, 0, true) == :error
+
+    # Negative value transfer
+    assert FS.Transfer.multi_transfer(id1, [id2, id3, id4], 986, -4242, true) == :error
+
+    # Bad currency
+    assert FS.Transfer.multi_transfer(id1, [id2, id3, id4], 123, 4242, true) == :error
+    assert FS.Transfer.multi_transfer(id1, [id2, id3, id4], "ABC", 4242, true) == :error
+
+    assert FS.delete_client(id1)
+    assert FS.delete_client(id2)
+    assert FS.delete_client(id3)
+    assert FS.delete_client(id4)
+    Supervisor.terminate_child(FS.Supervisor, Register)
+  end
+
+  test "Multi_transfer/3 ", %{registry: _registry} do
+    assert {client_pid1, id1} = FS.create_client("toto", 986, 4242)
+    assert {client_pid2, id2} = FS.create_client("titi", 986, 101_010)
+    assert {client_pid3, id3} = FS.create_client("tata", 978, 105_252.00)
+
+    ## Valid Tests
+
+    # Different clients, same currency
+    assert FS.Transfer.multi_transfer(id1 + 986, [id2 + 986, id3 + 986], 4242) == [:ok, :ok]
+
+    # Different clients, different currency
+    assert FS.Transfer.multi_transfer(id3 + 978, [id1 + 986, id2 + 124], 4242) == [:ok, :ok]
+
+    # Same client, same currency
+    assert FS.Transfer.multi_transfer(id1 + 986, [id1 + 986, id1 + 986], 1000) == [:ok, :ok]
+
+    # Same client, different currency
+    assert FS.Transfer.multi_transfer(id1 + 986, [id1 + 978, id1 + 124], 100) == [:ok, :ok]
+
+    ## Error Tests
+
+    # Invalid transfer amount
+    assert FS.Transfer.multi_transfer(id1 + 986, [id2 + 986, id3 + 978], 0) == :error
+    assert FS.Transfer.multi_transfer(id1 + 986, [id2 + 986, id3 + 978], -4242) == :error
+
+    # Unknown client
+    assert FS.Transfer.multi_transfer(8000 + 986, [id2 + 986, id3 + 978], 4242) == :error
+
+    assert FS.Transfer.multi_transfer(id1 + 986, [9000 + 986, id2 + 986, 12000 + 124], 4242) ==
+             :error
+
+    # Unknown currency
+    assert FS.Transfer.multi_transfer(id1 + 123, [id2 + 986, id3 + 978], 4242) == :error
+
+    assert FS.Transfer.multi_transfer(id1 + 986, [id2 + 123, id3 + 124, id1 + 321], 4242) ==
+             :error
+
+    assert FS.delete_client(id1)
+    assert FS.delete_client(id2)
+    assert FS.delete_client(id3)
     Supervisor.terminate_child(FS.Supervisor, Register)
   end
 end
