31ce2f0d9cdec9f4235aeef00a88ed82f310a2a0
2ndcouteau <17924220+2ndcouteau@users.noreply.github.com>
2019-05-21 19:58:06 +0200

    feat: Add a fresh API_key

--- lib/secret/fixer_api.ex
+++ lib/secret/fixer_api.ex
@@ -12,12 +12,16 @@ defmodule FS.Fixer_API do
 
   # julag@simpleemail.info == "8be9b6f827c2c8f88571572481b20002"
   # tupomiri@quick-mail.club == "2c5f01fdc85d23fcad6c1a716eba0d0e"
+  ## ~~ 500/1000
+  # FRESH KEY
+  # xusoroho@quick-mail.club == "3770527b3520ce3d571f289a4bb9e309"
   defmacro __using__(_) do
     quote do
       ## OLD KEYS
       # @key_api "8be9b6f827c2c8f88571572481b20002"
+      # @key_api "2c5f01fdc85d23fcad6c1a716eba0d0e"
       ## FRESH ONES
-      @key_api "2c5f01fdc85d23fcad6c1a716eba0d0e"
+      @key_api "3770527b3520ce3d571f289a4bb9e309"
     end
   end
 end
